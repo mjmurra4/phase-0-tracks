0d949a0710c360f302c2adf34cf59de0fc36fe9e
Martin Murray <mjmurra4@gmail.com>
2016-09-28 19:50:09 -0700

    release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,29 @@ class Puppy
     toy
   end
 
+  def speak(x)
+  	puts 'Woof!' * x
+  	x
+  end
+
+  def roll_over
+  	puts '*rolls over*'
+  end
+
+  def dog_years(x)
+  	puts x * 7
+  	x
+  end
+
+  def paw(treat)
+  	if treat == 'yes'
+  		puts '*raises paw*'
+  	else
+  		puts '*sleeps*'
+  	end
+  end
 end
+
+spot = Puppy.new
+spot.paw('brocolli')
 
