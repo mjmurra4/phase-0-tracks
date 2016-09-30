6bda652afd5d71c72615f2175c66c4350079e7a3
Martin Murray <mjmurra4@gmail.com>
2016-09-29 23:29:03 -0700

    6.4

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,38 @@
+#module Shout
+#  def self.yell_angrily(words)
+#    words + "!!!" + " :("
+#  end
+#
+# def self.yell_happily(words)
+#    words + "!!!" + " :)" + "Woot!"
+#  end
+#
+#end
+#
+#p Shout.yell_angrily('words')
+#p Shout.yell_happily('words')
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
+ def yell_happily(words)
     words + "!!!" + " :)" + "Woot!"
   end
 
 end
 
-p Shout.yell_angrily('words')
-p Shout.yell_happily('words')
+class Trump
+	include Shout
+end
+
+class Kool_Aid_Man
+	include Shout
+end
+
+donald = Trump.new
+p donald.yell_angrily('WRONG')
+
+red = Kool_Aid_Man.new
+p red.yell_happily('Oh Yeah')
